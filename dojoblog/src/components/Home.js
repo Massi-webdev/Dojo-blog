d2087b921cd6838c68113a9de7d14fec1cf66881
Massi-webdev <ouiddir.massia@gmail.com>
2024-01-21 12:17:35 -0500

    added click events

--- dojoblog/src/components/Home.js
+++ dojoblog/src/components/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello Ninjas', e)
+    }
+    
+    const handleClickAgain = (name, e) => {
+        console.log('Hello, ' + name, e)
+    }
+
+
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e)=>{handleClickAgain('Massi', e)}}>Click me again</button>
         </div>
      );
 }
